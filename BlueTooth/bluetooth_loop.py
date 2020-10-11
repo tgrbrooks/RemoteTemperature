5ccb034fade7132f4e8dde0494b39c86eeae203c
Tom <tom.g.r.brooks@gmail.com>
2020-10-11 22:25:35 +0100

    Switch of bluetooth (blocks wifi) each loop

--- BlueTooth/bluetooth_loop.py
+++ BlueTooth/bluetooth_loop.py
@@ -1,7 +1,7 @@
-# Blue Maestro Tempo Disk Scanner for MQTT
-# Takes all values from the scanned tempo disc and using the MAC id pushes each topic into MQTT
-# requires paho.mqtt client and bluez.
-# Also requires bluemaestroscan.py in th esame directory as this has the specifics for decoding
+# Blue Maestro Tempo Disk Scanner
+# Takes all values from the scanned tempo disc and sends to a google sheet
+# requires pybluez and gspread.
+# Also requires bluemaestroscan.py in the same directory as this has the specifics for decoding
 #  the advertising packets for the Blue Maestro Tempo Disc https://www.bluemaestro.com/product/tempo-disc-temperature/
 # Unsure if there are any other Blue Maetro products that it would work with.
 # David@andc.nz 15/12/2016
@@ -13,35 +13,36 @@
 #	- uses "simple" MQTT with topics defined in config file
 #	- MQTT server info is defined in config file
 
+# Modified 2020-10 by tgrbrooks:
+#       - Doesn't use MQTT, sends to google sheet instead
+
 import bluemaestroscan  #specific bluemaestro tempo disc scanner
 import json
 import sys
 import time
 import bluetooth._bluetooth as bluez
+import csv
 import gspread
 
+# --------- USER CONFIGURATION -------------
+# How often to take a reading
+FREQUENCY_SECONDS = 30
+
+# Open up google sheet interface
+# Requires the API key in a json file at ~/.config/gspread/service_account.json
+# Also requires the sheet to be shared with the service account
 client = gspread.service_account()
 sheet = client.open('Remote Temperature')
 sheet_instance = sheet.get_worksheet(0)
 
 dev_id = 0
-FREQUENCY_SECONDS = 600
 
-def main():
-    # Set freq and log filename
-    FREQUENCY_SECONDS = 10
-	
-    # Set MQTT credentials, open connection and start background network loop
-    try:
-        sock = bluez.hci_open_dev(dev_id)
-
-    except:
-        print("error accessing bluetooth device...")
-        sys.exit(1)
-
+def main(sock):
+    # Set up for scanning
     bluemaestroscan.hci_le_set_scan_parameters(sock)
     bluemaestroscan.hci_enable_le_scan(sock)
 
+    # Loop until killed
     while True:
         try:
             returnedList = bluemaestroscan.parse_events( sock, 10 )
@@ -51,7 +52,7 @@ def main():
             date_time = currentdatetime.split('T')
 
             for beacon in returnedList:
-                # Publish to the MQTT channel
+                # Publish to the google sheet
                 try:
                     temp = float( beacon["temp"] )
                     humidity = float( beacon["humidity"] )
@@ -59,12 +60,18 @@ def main():
                     battery = beacon["battery"]
 
                     print("Temp: {0} C, humidity: {1}%, dewpoint: {2} C, battery: {3}%".format( temp, humidity, dewpoint, battery ))
-                    # Write to file somewhere or direct to sheets
+                    # Write to file somewhere and direct to sheet
+                    bluemaestroscan.hci_disable_le_scan(sock)
                     row = [date_time[0], date_time[1], temp, humidity, dewpoint, battery]
+                    with open("/home/pi/Documents/RemoteTemperature/data.csv", "a") as csvfile:
+                        writer = csv.writer(csvfile, delimiter=',')
+                        writer.writerow(row)
                     # Get rid of a row if there are too many
                     if sheet_instance.row_count > 5000:
                         sheet_instance.delete_row(2)
                     sheet_instance.append_row(row)
+                    time.sleep( FREQUENCY_SECONDS )
+                    bluemaestroscan.hci_enable_le_scan(sock)
 
                 except Exception as e:
                     # Null out the worksheet so a login is performed at the top of the loop.
@@ -87,6 +94,10 @@ def main():
 
 if __name__ == '__main__':
     try:
-        main()
+        sock = bluez.hci_open_dev(dev_id)
+        main(sock)
     except (KeyboardInterrupt, SystemExit):
+        bluemaestroscan.hci_disable_le_scan(sock)
+        sock.close()
         print("Top-level exception - terminating.")
+        sys.exit(1)
